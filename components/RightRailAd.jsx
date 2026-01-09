e2b98da34796b7a4ff4e051d3a36799fa5f62b31
sujithannabomina <sujith.boss01@gmail.com>
2026-01-09 22:06:21 +0530

    Delete components directory

--- components/RightRailAd.jsx
+++
@@ -1,9 +0,0 @@
-// components/RightRailAd.jsx
-export default function RightRailAd() {
-  return (
-    <div className="rounded-xl border border-gray-800 bg-black/40 p-4">
-      <div className="text-lg font-semibold mb-2">Ads</div>
-      <p className="text-gray-400">Ads will appear here and across the page (Auto Ads).</p>
-    </div>
-  );
-}
